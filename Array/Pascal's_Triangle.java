dcafd72921a8d5f1e0799096f9bbb31c886300bc
aditya123-wdev <adityaredmi5a1597@gmail.com>
2026-04-09 01:56:09 +0530

    Create Pascal's_Triangle.java

---
+++ Array/Pascal's_Triangle.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> generate1(int numRows) {
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    int val = triangle.get(i - 1).get(j - 1) +
+                              triangle.get(i - 1).get(j);
+                    row.add(val);
+                }
+            }
+            triangle.add(row);
+        }
+
+        return triangle;
+    }
+    public List<List<Integer>> generate(int numRows) {
+        return generate1(numRows);
+    }
+}
